79bd68a071122451b5d7049acc8a2e92aadbe7c6
Jordi <124367342+JotaBeauProgramacion@users.noreply.github.com>
2023-03-07 14:40:29 +0100

    Create Persona.java

---
+++ Persona.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Persona {
+   private int edad;
+   private String nombre;
+   private int telefono;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public Persona() {
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "edad=" + edad + ", nombre=" + nombre + ", telefono=" + telefono + '}';
+    }
+   
+   
+}
